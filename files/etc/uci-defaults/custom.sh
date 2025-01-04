770694fbe6ddd61a254b70d5434e8151ee01945a
AJ <983097239@qq.com>
2025-01-04 16:15:51 +0800

    Update custom.sh

--- files/etc/uci-defaults/custom.sh
+++ files/etc/uci-defaults/custom.sh
@@ -21,9 +21,10 @@ done
 
 # 网络设置
 if [ "$count" -eq 1 ]; then
-  uci set network.lan.proto='dhcp'
+  #uci set network.lan.proto='dhcp'
+  uci set network.lan.ipaddr='192.168.5.2'
 elif [ "$count" -gt 1 ]; then
-  uci set network.lan.ipaddr='192.168.100.1'
+  uci set network.lan.ipaddr='192.168.5.2'
 fi
 
 # 设置所有网口可访问网页终端
